f9d70d5befef64437653fac60ce164944f35a543
dpkruczek <daniel.philippe.kruczek@gmail.com>
2024-05-08 10:30:00 +0200

    chore: better example question for csv agent

--- rag/csv_agent_test.py
+++ rag/csv_agent_test.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_agent
 from settings import OPENAI_API_KEY
-import pandas as pd
+# import pandas as pd
 
 # df = pd.read_csv('./corpus/employees.csv', sep=';', encoding='latin-1')
 # print(df.head())
@@ -15,4 +15,7 @@ agent = create_csv_agent(
     pandas_kwargs={"encoding": "latin-1", "sep": ";"},
     verbose=True)
 
-agent.run("What is the gender ratio for each department?")
+agent.run("What is the attrition rate for the last few years? The current date is 2024 May 06. You can assume that "
+          "for any given date, if the date you are looking at is between the startDate and lastEmploymentDate, "
+          "then that person is employed. If an employee does not have a lastEmploymentDate, the person is still "
+          "employed")
